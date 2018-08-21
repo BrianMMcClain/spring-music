53c4fde8ed05099b06099b5918314d17e43f780f
Brian McClain <brianmmcclain@gmail.com>
2018-08-21 13:35:24 -0400

    Update build script

--- ci/build.sh
+++ ci/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
-spring-music-app/gradlew --build-file spring-music-app/build.gradle clean assemble
-cp spring-music-app/build/libs/*.jar build-output/
+
+cd spring-music-app
+./gradlew clean assemble
+cp ./build/libs/*.jar ../build-output/
